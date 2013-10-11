6775f7a85900c504eb9964fbf7c77508ad5a8826
jinkai <jinkai1987@gmail.com>
2013-10-11 14:15:28 +0800

    Initial Commit

---
+++ ExquisiteST/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ExquisiteST
+//
+//  Created by kim on 13-10-11.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ ExquisiteSTTests/ExquisiteSTTests.m
@@ -0,0 +1,34 @@
+//
+//  ExquisiteSTTests.m
+//  ExquisiteSTTests
+//
+//  Created by kim on 13-10-11.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ExquisiteSTTests : XCTestCase
+
+@end
+
+@implementation ExquisiteSTTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
